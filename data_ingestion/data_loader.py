354e1a870648ab6bf12b8941ad52e3e7dbd2e55d
bibek <Bibekanandan892@gmail.com>
2022-02-27 02:09:08 +0530

    Initial commit

---
+++ data_ingestion/data_loader.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+# =============================================================================
+# # ---------------------------------------------------------------------------
+# # Creation Date: 12-jan-2021
+# # Author: Bibekananda Nayak
+# # Version: 1.0
+# # This class shall  be used for obtaining the data from the source for training.
+# # ---------------------------------------------------------------------------#
+# =============================================================================#
+
+
+class Data_Getter:
+
+    def __init__(self, file_object, logger_object):
+        self.training_file='Training_FileFromDB/InputFile.csv'
+        self.file_object=file_object
+        self.logger_object=logger_object
+
+    def get_data(self):
+        """
+        Method Name: get_data
+        Description: This method reads the data from source.
+        Output: A pandas DataFrame.
+        On Failure: Raise Exception
+
+        """
+        self.logger_object.log(self.file_object,'Entered the get_data method of the Data_Getter class')
+        try:
+            self.data= pd.read_csv(self.training_file) # reading the data file
+            self.logger_object.log(self.file_object,'Data Load Successful.Exited the get_data method of the Data_Getter class')
+            return self.data
+        except Exception as e:
+            self.logger_object.log(self.file_object,'Exception occured in get_data method of the Data_Getter class. Exception message: '+str(e))
+            self.logger_object.log(self.file_object,
+                                   'Data Load Unsuccessful.Exited the get_data method of the Data_Getter class')
+            raise Exception()
+
+
